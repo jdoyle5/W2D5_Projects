d6851ab9b7335f92b1602ca9dfc050ca5be25bae
Joey Doyle <j.doyle345@gmail.com>
2017-10-13 15:36:45 -0700

    Created hash methods for Array, String, and Hash: Phase II

--- lib/p02_hashing.rb
+++ lib/p02_hashing.rb
@@ -2,13 +2,27 @@ class Fixnum
   # Fixnum#hash already implemented for you
 end
 
+# create hashes, with # of index as keys, and items as values such as "hello", 5, :blah
+# {0=> "hello", 1> 5, 2=> :blah}
+
+
 class Array
   def hash
+    result = 0
+    self.each_with_index do |el, i|
+      result += (el.hash * i.hash)
+    end
+    result
   end
 end
 
 class String
   def hash
+    result = 0
+    self.chars.each_with_index do |char, i|
+      result += (char.ord.hash * i.hash)
+    end
+    result
   end
 end
 
@@ -16,6 +30,7 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    self.to_a.sort.flatten.hash
+    # 0
   end
 end
